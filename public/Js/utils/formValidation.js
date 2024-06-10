1684963d451cc512dbc82419d7d6c2a3bf8e6723
George-oikonomou <giorgos@workadu.com>
2024-06-10 14:53:07 +0300

    fixed some stuff

--- public/Js/utils/formValidation.js
+++ public/Js/utils/formValidation.js
@@ -20,7 +20,7 @@ export const getBookData = () => {
     const author = ELEMENTS.author.value.trim();
     const title = ELEMENTS.title.value.trim();
     const genre = ELEMENTS.genreSelect.value;
-    const price = parseFloat(ELEMENTS.price.value.trim()); // Parse price as a float
+    const price = ELEMENTS.price.value.trim();
 
     return { author, title, genre, price };
 };
@@ -29,11 +29,19 @@ export const getFormValidationErrors = async (translations) => {
     const errors = [];
     const {author, title, genre, price} = getBookData();
 
-    if (!author) errors.push(translations.empty_author_error);
+    if (!author )
+        errors.push(translations.empty_author_error);
+    else if (author && author.length < 3 && author.length > 50)
+        errors.push(translations.author_length_error);
+
+    if (/\d/.test(author)) {
+        errors.push(translations.author_number_error);
+    }
+
 
     if (!title)
         errors.push(translations.empty_title_error);
-    else if (title && title.length < 3)
+    else if (title && title.length < 3 && title.length > 100)
         errors.push(translations.title_length_error);
 
     if (await bookExists({author, title}))
@@ -41,9 +49,11 @@ export const getFormValidationErrors = async (translations) => {
 
     if (!genre) errors.push(translations.empty_genre_error);
 
-    if (price == null)
+    if (price === "")
         errors.push(translations.empty_price_error);
-    else if (isNaN(price) || price <= 0.01 || price > CONFIG.MAX_PRICE)
+    else if (price && !/^\d+(\.\d{1,2})?$/.test(price))
+        errors.push(translations.invalid_price_type_error);
+    else if (isNaN(price) || price < 0.01 || price > CONFIG.MAX_PRICE)
         errors.push(translations.invalid_price_error);
 
     return errors.length ? errors : null;
